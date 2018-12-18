6e62a0313314bc0e04a9339c4c0a390a1bb496c6
Faraj Daoud <faraj.daoud@gmail.com>
2018-12-18 15:45:56 -0500

    finalizing project

--- src/index.js
+++ src/index.js
@@ -22,9 +22,9 @@ req.onload = function(){
     ,maxTime = d3.max(dataset.map((x) => x.Time))
     ,minTime = d3.min(dataset.map((x) => x.Time));
     console.log(`maxYear: ${maxYear} minYear: ${minYear} maxTime: ${maxTime} minTime: ${minTime}`);
-    const w = document.getElementById('spgContainer').offsetWidth
-    , h = 500
-    ,padding = 40;
+    const w = 800
+    , h = 600
+    ,padding = 60;
 
     const xScale = d3.scaleLinear()
                     .domain([minYear - 1, maxYear + 1])
@@ -45,10 +45,7 @@ req.onload = function(){
         .attr('id', 'tooltip')
         .style('opacity', 0);
 
-    const legend = d3.select('#scatterPlotGraph').append('div')
-                .attr('id', 'legend')
-                .style('left', w - 250 + 'px')
-                .style('top', h/2 + 'px');
+    const legendScale = d3.scaleOrdinal(d3.schemeDark2);
 
     svg.append("g")
             .attr("transform", `translate(0, ${h - padding})`)
@@ -60,6 +57,13 @@ req.onload = function(){
             .attr("transform", `translate(${padding}, 0)`)
             .attr("id", "y-axis")
             .call(yAxis);
+        
+    svg.append('text')
+        .attr('x', -200)
+        .attr('y', 15)
+        .attr('transform', 'rotate(-90)')
+        .style('font-size', 19)
+        .text('Time in Minutes');
 
     svg.selectAll(".dot")
         .data(dataset)
@@ -68,10 +72,10 @@ req.onload = function(){
         .attr('class', 'dot')
         .attr('cx', (d) => xScale(d.Year))
         .attr('cy', (d) => yScale(d.Time))
-        .attr('r', (d) => 7)
+        .attr('r', (d) => 5)
         .attr('data-xvalue', (d) => d.Year)
         .attr('data-yvalue', (d) => d.Time)
-        .style('fill', (d) => d.Doping ? 'red' : 'green')
+        .style('fill', (d) => legendScale(d.Doping != ''))
         .style('opacity', .5)
         .on('mouseover', (d) => {
             tooltip.style('opacity', .9)
@@ -89,5 +93,26 @@ req.onload = function(){
                 .style('z-index', -1)
         });
 
+    var legend = svg.selectAll('#legend')
+    .data(legendScale.domain())
+    .enter()
+    .append('g')
+    .attr('id', 'legend')
+    .attr('transform', (d, i) => {
+        return 'translate(0, ' + (h/2 - i * 25) + ')'
+    });
+
+    legend.append('rect')
+        .attr('x', w - 18)
+        .attr('width', 18)
+        .attr('height', 18)
+        .style('fill', legendScale);
+    
+    legend.append("text")
+        .attr("x", w - 24)
+        .attr("y", 9)
+        .attr("dy", ".35em")
+        .style("text-anchor", "end")
+        .text((d) => d ? "Riders with doping allegations" : "No doping allegations");
     
 };
